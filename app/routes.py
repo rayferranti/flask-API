b1be059610c32c3224afbc9dd1736b10d3ca8f94
rayferranti <raymondferranti0124@gmail.com>
2017-12-08 13:48:38 -0500

    route now returns csv file transformed into a json object

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
+import pandas as pd
+from flask import jsonify
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello"
+    csv = pd.read_csv('data/ufo.csv')
+    return jsonify(csv.T.to_dict().values())
